8242b3290a18855019f4a0bb67e4ab1a8991c0ed
David Aslan <webmaster@aston9.com>
2021-11-14 15:28:58 +0100

    Handle return of JSON response

--- tautulliApi/main.py
+++ tautulliApi/main.py
@@ -6,11 +6,16 @@ IP = ""
 TOKEN = ""
 
 
-async def async_example():
+async def main():
     host_configuration = PyTautulliHostConfiguration(ipaddress=IP, api_token=TOKEN)
     async with PyTautulli(host_configuration=host_configuration) as client:
         print(await client.async_command("get_activity"))
-
+        z = str(await client.async_command("get_activity"))
+        z = z.split("sessions", 1)[0][:-3]
+        for i in range(z.count('{')):
+            z += "}"
+        z = z.replace("'", '"').replace("True", '"True"').replace("False", '"False"').replace("None", '"None"')
 
 asyncio.get_event_loop().run_until_complete(async_example())
 
+asyncio.get_event_loop().run_until_complete(main())
